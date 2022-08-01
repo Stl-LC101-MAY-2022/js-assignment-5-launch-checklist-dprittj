7f39ffb105d6f458ff5348f335b6b1b051b0f3ce
dprittj <108655696+dprittj@users.noreply.github.com>
2022-08-01 17:54:17 -0500

    Getting there

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+const { validateInput, formSubmission } = require("./scriptHelper");
 
+
+window.addEventListener("load", function() {
+    
+    //     validateInput();
+    //     });
+    // let button = document.querySelector("formSubmit");
+    // button.addEventListener("click", function(){
+    //     validateInput(button);
+    //     formSubmission(button);
+    // });
+    
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
@@ -13,4 +25,20 @@ window.addEventListener("load", function() {
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
    
+    let form = document.querySelector("form");
+    form.addEventListener("click", function(){
+        let pilotInput = document.querySelector("input[name=pilotName]");
+        let pilot = pilotInput.value;
+
+        let copilotInput = document.querySelector("input[name=copilotName]");
+        let copilot = copilotInput.value;
+
+        let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
+        let fuel = fuelLevelInput.value;
+
+        let cargoMassInput = document.querySelector("input[name=cargoMass]");
+        let cargo = cargoMassInput.value;
+        
+    })
+
 });
